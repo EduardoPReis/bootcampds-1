eb45837084cb6c0e5a4b23bd583b7b5ae004aef4
Eduardo Reis <reisprogramer@gmail.com>
2022-10-21 12:41:10 -0300

    correção de dependencias de gradlle para maven

---
+++ backand/src/main/java/com/bootecampdsdesafioum/demo/BootecampdsdesafioumApplication.java
@@ -0,0 +1,13 @@
+package com.bootecampdsdesafioum.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootecampdsdesafioumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootecampdsdesafioumApplication.class, args);
+	}
+
+}
---
+++ backand/src/test/java/com/bootecampdsdesafioum/demo/BootecampdsdesafioumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootecampdsdesafioum.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootecampdsdesafioumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
